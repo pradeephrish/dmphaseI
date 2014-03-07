fc662709e48bcf6bbcedcb29dc5a71dcc2bbfb3e
ryavied <ravindra.dingankar@asu.edu>
2014-03-07 02:21:41 -0700

    saving model to file, logger in progress

--- DM/src/com/dm/file/SaveToFile.java
+++ DM/src/com/dm/file/SaveToFile.java
@@ -1,5 +1,10 @@
 package com.dm.file;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import com.dm.model.SnopesModel;
 
 public class SaveToFile {
@@ -8,12 +13,63 @@ public class SaveToFile {
 		model.setClaim("");
 		model.setClaim("Evacuees from Louisiana, Mississippi, or Alabama can get two months' of food stamps and free gasoline from the Texas DHHS by showing their driver's licenses.");
 		//set other fields
-		String url = "url from sample";
-	}
-	/*
-	 * Shoudld save to txt file - derive file name from url
-	 */
-	public void saveToFile(SnopesModel snopesModel,String url){
 		
+		model.setOrigins("");
+		model.setOrigins("Evacuees from Louisiana, Mississippi, or Alabama can get two months' of food stamps and free gasoline from the Texas DHHS by showing their driver's licenses.");
+		//set other fields
+		
+		model.setStatus("");
+		model.setStatus("Evacuees from Louisiana, Mississippi, or Alabama can get two months' of food stamps and free gasoline from the Texas DHHS by showing their driver's licenses.");
+		//set other fields
+		
+		model.setExample("");
+		model.setExample("Evacuees from Louisiana, Mississippi, or Alabama can get two months' of food stamps and free gasoline from the Texas DHHS by showing their driver's licenses.");
+		//set other fields
+		String url = "http://www.snopes.com/katrina/charity/foodstamps.asp";
+		
+		saveToFile(model, url);
+	}
+
+	public static void saveToFile(SnopesModel snopesModel,String url){
+		
+//		get file name
+		int startIndex = url.indexOf('/', 15) + 1;
+		int endIndex = url.indexOf('.', url.length() - 8);
+		String path = url.substring(startIndex, endIndex);
+		String fName = path.replace('/', '-') + ".txt";
+		System.out.println(fName);
+		
+//	if file doesnt exist, create new file 	
+		File file = new File("data/" + fName);
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+//		write data to file
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+			bw.write("@@@begin_claim@@@" + snopesModel.getClaim() + "@@@end_claim@@@");
+			bw.newLine();
+			bw.write("@@@begin_status@@@" + snopesModel.getClaim() + "@@@end_status@@@");
+			bw.newLine();
+			bw.write("@@@begin_example@@@" + snopesModel.getClaim() + "@@@end_example@@@");
+			bw.newLine();
+			bw.write("@@@begin_origin@@@" + snopesModel.getClaim() + "@@@end_origin@@@");
+			bw.newLine();
+			bw.write("@@@begin_sources@@@" + snopesModel.getClaim() + "@@@end_sources@@@");
+			
+			bw.flush();
+			bw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
 	}
 }
---
+++ DM/src/com/dm/logger/Logger.java
@@ -0,0 +1,9 @@
+package com.dm.logger;
+
+public class Logger {
+	
+	public static void log( String str){
+		
+	}
+
+}
